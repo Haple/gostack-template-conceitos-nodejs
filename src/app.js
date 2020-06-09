4af5a4548423642d8d19118b5b2b8d62f380aac8
haple <aleph.oliveira@etec.sp.gov.br>
2020-06-09 13:04:09 -0300

    Challenge completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,46 @@ app.use(cors());
 
 const repositories = [];
 
+function findRepository(req, res, next) {
+  const { id } = req.params;
+  const repository = repositories.find(repository => repository.id == id);
+  if (!repository)
+    return res.status(400).json({ error: "Repository not found" });
+  req.repository = repository;
+  next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    likes: 0,
+    title, url, techs,
+  };
+  repositories.push(repository);
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", findRepository, (request, response) => {
+  const { title, url, techs } = request.body;
+  request.repository.title = title;
+  request.repository.url = url;
+  request.repository.techs = techs;
+  return response.json(request.repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", findRepository, (request, response) => {
+  repositories.splice(repositories.indexOf(request.repository), 1);
+  return response.status(204).json();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", findRepository, (request, response) => {
+  request.repository.likes++;
+  return response.json(request.repository);
 });
 
 module.exports = app;
